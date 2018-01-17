9ec33b91723d35705927886169da202109972f69
Hunter Matthews <huntermatthews@Hunters-MacBook-Pro.local>
2018-01-17 00:04:57 -0800

    Clean up code and restructure randomizing block

--- CubeScramble.py
+++ CubeScramble.py
@@ -5,28 +5,17 @@
 # Here is what end result should look like:
 # B2  D  R'  U'  F'  R  U'  B  R  B'  R2  L  D2  U2  B  R'  U'  R  B2  F  D'  F  D  F2  U'
 
-
 import random
 
-
 # all possible moves on a Rubik's Cube
-list = ['U', 'Ui', 'U2', 'F', 'Fi', 'F2', 'R', 'Ri', 'R2', 'B', 'Bi', 'B2', \
-    'L', 'Li', 'L2', 'D', 'Di', 'D2']
+list = ['U', 'Ui', 'U2', 'F', 'Fi', 'F2', 'R', 'Ri', 'R2', 'B', 'Bi', 'B2',
+        'L', 'Li', 'L2', 'D', 'Di', 'D2']
 
 # shuffle those moves
 scrambleLength = range(25)
 scrambleAlg = []
-random.shuffle(list)
 
 for i in scrambleLength:
-    if i < 18:
-        scrambleAlg.append(list[i])
-        print scrambleAlg
-    else:
-        print 'sorry'
+    scrambleAlg.append(random.choice(list))
 
-
-# print shuffled list
-
-#print list
-#print len(list)
+print scrambleAlg
