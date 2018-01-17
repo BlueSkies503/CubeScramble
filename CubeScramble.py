d0cbba28b2401212b568f2ae1bc19fbe4efa8a4c
Hunter Matthews <huntermatthews@Hunters-MacBook-Pro.local>
2018-01-16 23:44:56 -0800

    Start to develop ideas on scrambling list

--- CubeScramble.py
+++ CubeScramble.py
@@ -1,23 +1,32 @@
 # Create a Rubik's Cube Scramble Generator
 # Scramble should be 25 moves long
 # Scramble should not have moves that make previous moves redundant
-# Example: R, Ri or R2
-
-
+# ^Example: R, Ri or R2, Ri (R2 + Ri = R)
+# Here is what end result should look like:
+# B2  D  R'  U'  F'  R  U'  B  R  B'  R2  L  D2  U2  B  R'  U'  R  B2  F  D'  F  D  F2  U'
 
 
 import random
 
 
 # all possible moves on a Rubik's Cube
-
 list = ['U', 'Ui', 'U2', 'F', 'Fi', 'F2', 'R', 'Ri', 'R2', 'B', 'Bi', 'B2', \
     'L', 'Li', 'L2', 'D', 'Di', 'D2']
 
 # shuffle those moves
-
+scrambleLength = range(25)
+scrambleAlg = []
 random.shuffle(list)
+
+for i in scrambleLength:
+    if i < 18:
+        scrambleAlg.append(list[i])
+        print scrambleAlg
+    else:
+        print 'sorry'
+
 
 # print shuffled list
 
-print list
+#print list
+#print len(list)
