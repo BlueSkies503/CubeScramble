c5b915957d0f5a908de6e28076bd48a115a6eaa8
Jesse Hix <jessehix@yahoo.com>
2018-01-18 00:39:00 -0800

    Update move generating algorithm

--- CubeScramble.py
+++ CubeScramble.py
@@ -4,20 +4,22 @@
 # Example: R, Ri or R2
 
 
-
-
-import random
-
+from random import randint
 
 # all possible moves on a Rubik's Cube
 
-list = ['U', 'Ui', 'U2', 'F', 'Fi', 'F2', 'R', 'Ri', 'R2', 'B', 'Bi', 'B2', \
+list = ['U', 'Ui', 'U2', 'F', 'Fi', 'F2', 'R', 'Ri', 'R2', 'B', 'Bi', 'B2',
     'L', 'Li', 'L2', 'D', 'Di', 'D2']
 
 # shuffle those moves
 
-random.shuffle(list)
+
+def generate_random_moves(num_moves):
+    moves = []
+    for move in xrange(num_moves):
+        moves.append(list[randint(0, len(list)) - 1])
+    return moves
+
 
 # print shuffled list
-
-print list
+print generate_random_moves(4)
